235598888471be6d9e5242a504e8c1c919148567
Justin <justinpeng15@gmail.com>
2022-05-23 12:48:32 -0600

    added not file constants to bitboard.h

--- bitboard.h
+++ bitboard.h
@@ -1,3 +1,7 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
+
 //define bitboard data type
 #define U64 unsigned long long
 
@@ -5,6 +9,9 @@
 #define set_bit(bitboard, square) (bitboard |= (1ULL << square))
 #define get_bit(bitboard, square) (bitboard & (1ULL << square))
 #define pop_bit(bitboard, square) (get_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0)
+
+// sides to move (colors)
+enum {white, black};
 
 // board squares
 enum {
@@ -18,4 +25,70 @@ enum {
     a1, b1, c1, d1, e1, f1, g1, h1
 };
 
+
+/**********************\
+    not A file
+ 
+  8  0 1 1 1 1 1 1 1
+  7  0 1 1 1 1 1 1 1
+  6  0 1 1 1 1 1 1 1
+  5  0 1 1 1 1 1 1 1 
+  4  0 1 1 1 1 1 1 1 
+  3  0 1 1 1 1 1 1 1
+  2  0 1 1 1 1 1 1 1
+  1  0 1 1 1 1 1 1 1
+
+     a b c d e f g h
+
+
+    not H file
+  8  1 1 1 1 1 1 1 0
+  7  1 1 1 1 1 1 1 0
+  6  1 1 1 1 1 1 1 0
+  5  1 1 1 1 1 1 1 0 
+  4  1 1 1 1 1 1 1 0 
+  3  1 1 1 1 1 1 1 0
+  2  1 1 1 1 1 1 1 0
+  1  1 1 1 1 1 1 1 0
+
+     a b c d e f g h
+
+    not HG file
+  8  1 1 1 1 1 1 0 0
+  7  1 1 1 1 1 1 0 0
+  6  1 1 1 1 1 1 0 0
+  5  1 1 1 1 1 1 0 0 
+  4  1 1 1 1 1 1 0 0 
+  3  1 1 1 1 1 1 0 0
+  2  1 1 1 1 1 1 0 0
+  1  1 1 1 1 1 1 0 0
+
+     a b c d e f g h
+
+    not AB file
+  8  0 0 1 1 1 1 1 1
+  7  0 0 1 1 1 1 1 1 
+  6  0 0 1 1 1 1 1 1
+  5  0 0 1 1 1 1 1 1 
+  4  0 0 1 1 1 1 1 1 
+  3  0 0 1 1 1 1 1 1
+  2  0 0 1 1 1 1 1 1
+  1  0 0 1 1 1 1 1 1
+
+     a b c d e f g h
+**********************/
+
+// not A file constant
+const U64 not_a_file = 18374403900871474942ULL;
+
+// not H file constant
+const U64 not_h_file = 9187201950435737471ULL;
+
+// not HG file constant
+const U64 not_hg_file = 4557430888798830399ULL;
+
+// not AB file constant
+const U64 not_ab_file = 18229723555195321596ULL;
 void print_bitboard(U64 bitboard);
+
+#endif
