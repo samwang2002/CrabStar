53c873786905f5a125bba52d94a3a50f06585131
Justin <justinpeng15@gmail.com>
2022-05-23 10:46:11 -0600

    implemented bitboard

--- bitboard.cpp
+++ bitboard.cpp
@@ -1,7 +1,45 @@
+// headers
 #include <iostream>
+#include "bitboard.h"
 
-#define U64 unsigned long long;
 
-int main() {
-    std::cout << "test\n";
+/**********************************\
+====================================
+
+        Bit manipulations
+
+====================================
+\**********************************/
+
+// print bitboard
+void print_bitboard(U64 bitboard)
+{
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // convert file & rank into square index
+            int square = rank * 8 + file;
+
+            // print ranks
+            if (!file)
+                std::cout << "  " << (8 - rank) << " ";
+
+            // print bit state (either 1 or 0)
+            std::cout << " " << (get_bit(bitboard, square) ? 1 : 0 );
+
+        }
+
+        // print new line every rank
+        std::cout << "\n";
+    }
+
+    // print board files
+    std::cout << "\n     a b c d e f g h\n\n";
+
+    // print bitboard as unsigned decimal number
+    std::cout << "     Bitboard: " << bitboard << "d\n" << std::endl;
+
 }
--- bitboard.h
+++ bitboard.h
@@ -1,35 +1,21 @@
-// system headers
-#include <iostream>
-
 //define bitboard data type
 #define U64 unsigned long long
 
-/**********************************\
-====================================
+// set/get/pop macros
+#define set_bit(bitboard, square) (bitboard |= (1ULL << square))
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+#define pop_bit(bitboard, square) (get_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0)
 
-        Bit manipulations
+// board squares
+enum {
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1
+};
 
-====================================
-\**********************************/
-
-// print bitboard
-void print_bitboard(U64 bitboard)
-{
-    // loop over board ranks
-    for (int rank = 0; rank < 8; rank++)
-    {
-        // loop over board files
-        for (int file = 0; file < 8; file++)
-        {
-            // convert file & rank into square index
-            int square = rank * 8 + file;
-
-            //
-            std::cout << " " << square << " ";
-
-        }
-
-        // print new line every rank
-        std::cout << "\n";
-    }
-}
+void print_bitboard(U64 bitboard);
