f1749f22bc8b269edc1fd4dee499d4791d200591
Justin <justinpeng15@gmail.com>
2022-05-23 10:48:16 -0600

    bitboard testing

--- crabstar.cpp
+++ crabstar.cpp
@@ -6,7 +6,21 @@
 
 int main()
 {
+    //define bitboard
     U64 bitboard = 0ULL;
+
+    // setting some bits
+    set_bit(bitboard, e4);
+    set_bit(bitboard, c3);
+    set_bit(bitboard, f2);
+
+    //print bitboard
+    print_bitboard(bitboard);
+
+    //reset bit
+    pop_bit(bitboard, e4);
+
+    //print bitboard
     print_bitboard(bitboard);
     
     return 0;
