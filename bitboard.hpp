c08ff138d32302e3d0286a1343bc84f301410ff1
Sam Wang <samwang2002@Sams-MacBook-Pro-2.local>
2022-06-17 16:19:54 -0500

    global constants can be static

--- bitboard.hpp
+++ bitboard.hpp
@@ -20,7 +20,7 @@
 
 /* ------------------------------- function declarations ------------------------------- */
 // get index of least significant bit
-static inline int get_ls1b_index(U64 bitboard);
+inline int get_ls1b_index(U64 bitboard);
 
 // print bitboard as grid of 0s and 1s
 void print_bitboard(U64 bitboard);
@@ -41,7 +41,16 @@ enum {
     a1, b1, c1, d1, e1, f1, g1, h1, no_sq
 };
 
-extern const char* square_to_coordinates[];
+static const char* square_to_coordinates[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+};
 
 // file constants
 const U64 not_a_file = 18374403900871474942ULL;
