9108fe63285a98d58075b0787a262e8c54176abf
Sam <13668208+samwang2002@users.noreply.github.com>
2022-06-25 21:29:53 -0500

    added all perft test positions, correct results

--- main.c
+++ main.c
@@ -19,23 +19,17 @@ int main()
 {
     init_attacks();
     // parse_fen(tricky_position);     // r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1
-    // parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N21qp/PPPBBPpP/R3K2R w KQkq - 0 1");
-    // parse_fen(start_position);
-    parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-    //parse_fen("8/8/8/8/8/k7/7q/K7 w - -");
+    // parse_fen(start_position);  // 1
+    // parse_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");  // 2
+    // parse_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - "); // 3
+    // parse_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");   // 4
+    // parse_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ");    // 5
+    parse_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ");  // 6
     print_board();
-
-
-    // int start = get_time_ms();
 
     // perft
     setlocale(LC_ALL, "");
-    printf("%d has legal moves: %d\n", side, has_legal_moves(side));
-    // side = white;
-    perft_test(3);
-    print_board();
-    // print_bitboard(bitboards[N]);
-    // getchar();
+    perft_test(5);
 
     return 0;
 }
