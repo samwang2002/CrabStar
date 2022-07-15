cd447de54b14d586a9990a7582bbb1a758ae7b6f
Sam <13668208+samwang2002@users.noreply.github.com>
2022-07-15 00:04:33 -0500

    fixed legality check

--- game.c
+++ game.c
@@ -6,33 +6,33 @@
 #include <stdio.h>
 
 // returns result of match between player1 (white) and player2 (black)
-int match(const net_weights *player1, const net_weights *player2, const int depth)
+int match(const net_weights *player1, const net_weights *player2, const int depth, const int verbose)
 {
     int move_count = 0;
-    move_list moves;
+    
     while (move_count++ < max_moves) {
         // white moves
         negamax(-infinity, infinity, depth, player1);
         make_move(pv_table[0][0], all_moves);
-        printf("%d ", move_count);
-        print_move(pv_table[0][0]);
-        printf(" ");
-        
-        // check if black has moves
-        generate_moves(&moves);
-        if (moves.count == 0) return (square_attacked(ls1b(bitboards[k]), white) ? 1 : 0);
+
+        if (verbose) {
+            printf("%d ", move_count);
+            print_move(pv_table[0][0]);
+            printf(" ");
+        }
+
+        if (!has_legal_moves()) return (square_attacked(ls1b(bitboards[k]), white) ? 1 : 0);
 
         // black moves
         negamax(-infinity, infinity, depth, player2);
         make_move(pv_table[0][0], all_moves);
-        print_move(pv_table[0][0]);
-        printf(" \n");
 
-        if (move_count % 10 == 0) print_board();
+        if (verbose) {
+            print_move(pv_table[0][0]);
+            printf("\n");
+        }
 
-        // check if white has moves
-        generate_moves(&moves);
-        if (moves.count == 0) return (square_attacked(ls1b(bitboards[K]), black) ? -1 : 0);
+        if (!has_legal_moves()) return (square_attacked(ls1b(bitboards[K]), black) ? -1 : 0);
     }
     return 0;
 }
--- game.h
+++ game.h
@@ -6,6 +6,6 @@
 #define max_moves 100
 
 // simulate game between two neural networks, return 1 for player 1 win, -1 for loss, 0 for draw
-int match(const net_weights *player1, const net_weights *player2, const int depth);
+int match(const net_weights *player1, const net_weights *player2, const int depth, const int verbose);
 
 #endif
