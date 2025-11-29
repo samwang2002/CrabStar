8c7c276e10dc9367d865de0420c9cdea807378e7
Sam Wang <13668208+samwang2002@users.noreply.github.com>
2025-11-29 19:38:28 +0000

    Increase engine depth from 6 to 8

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def make_move():
         board = chess.Board(fen)
 
         # FORCE a cheap test first
-        result = engine.play(board, chess.engine.Limit(depth=6, time=weight))
+        result = engine.play(board, chess.engine.Limit(depth=8, time=weight))
 
         board.push(result.move)
 
